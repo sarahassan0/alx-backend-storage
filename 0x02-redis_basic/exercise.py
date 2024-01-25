feb96c5fd032382bed9464e963ad6682f5ab43d4
sarahassan0 <sarraree5@gmail.com>
2024-01-25 04:26:07 +0200

    [add] count calls, call history

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -5,7 +5,38 @@
 import redis
 import uuid
 from typing import Union, Callable
+from functools import wraps
 
+
+def count_calls(method: Callable) -> Callable:
+    """Decorator to count the number of times a method is called."""
+
+    @wraps(method)
+    def wrapper(self, *args, **kwds):
+        """Wrapper function for the decorated method."""
+        if isinstance(self._redis, redis.Redis):
+            self._redis.incr(method.__qualname__)
+
+        return method(self, *args, **kwds)
+
+    return wrapper
+
+
+def call_history(method: Callable) -> Callable:
+    """Decorator to add input and output paramters of callable to 2 lists"""
+
+    @wraps(method)
+    def wrapper(self, *args, **kwds):
+        """Wrapper function for the decorated method."""
+        generated_key = method(self, *args, **kwds)
+
+        if isinstance(self._redis, redis.Redis):
+            self._redis.rpush(f"{method.__qualname__}:inputs", str(args))
+            self._redis.rpush(f"{method.__qualname__}:outputs", generated_key)
+
+        return generated_key
+
+    return wrapper
 
 class Cache:
     """A Cache class that stores data in Redis."""
